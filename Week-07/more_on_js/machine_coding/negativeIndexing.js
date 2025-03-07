9ffb46f47b04cafdc62cc408f24955d3436f2148
geeksamarth <samarthagrawal1804@gmail.com>
2025-03-07 23:59:30 +0530

    add: js code for machine coding of proxy in js

---
+++ Week-07/more_on_js/machine_coding/negativeIndexing.js
@@ -0,0 +1,27 @@
+function negativeIndex(arr) {
+  return new Proxy(arr, {
+    get(target, prop) {
+      const index = Number(prop);
+      if (index < 0) {
+        return target[target.length + index];
+      }
+      return target[index];
+    },
+    set(target, prop, value) {
+      const index = Number(prop);
+      if (index < 0) {
+        target[target.length + value] = value;
+      } else {
+        target[index] = value;
+      }
+      return true;
+    },
+  });
+}
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let newArr = negativeIndex(arr);
+console.log(arr[-1]);
+console.log(newArr[-1]);
+newArr[-1] = 25;
+console.log(newArr);
+console.log(arr);
---
+++ Week-07/more_on_js/machine_coding/script.js
@@ -0,0 +1,21 @@
+// Proxy in js
+
+const user = {
+  name: "Samarth",
+  password: 123,
+};
+
+const proxyUser = new Proxy(user, {
+  // get the info of proxied object
+  get(target, prop) {
+    console.log(prop);
+    if (prop === "password") {
+      throw new Error("Access Denied");
+    }
+    return target[prop];
+  },
+  // setting the value of proxied object
+  set(target,prop,value){}
+});
+
+console.log(proxyUser.name);
