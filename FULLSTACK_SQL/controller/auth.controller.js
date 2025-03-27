db5952c78dc4f808d991bfd57db9b70704da25ae
geeksamarth <samarthagrawal1804@gmail.com>
2025-03-28 01:00:37 +0530

    add: field in prisma schema and add something in register controller

--- FULLSTACK_SQL/controller/auth.controller.js
+++ FULLSTACK_SQL/controller/auth.controller.js
@@ -1,3 +1,7 @@
-export const registerUser = async(req,res)=>{
-console.log('user registered')
-}
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const registerUser = async (req, res) => {
+  
+};
