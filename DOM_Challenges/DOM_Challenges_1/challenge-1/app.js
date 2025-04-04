964096b999fe31a25ca59f19082eb0d0adb5bf50
geeksamarth <samarthagrawal1804@gmail.com>
2025-04-05 02:08:27 +0530

    add: js logic part to implement the functioning

--- DOM_Challenges/DOM_Challenges_1/challenge-1/app.js
+++ DOM_Challenges/DOM_Challenges_1/challenge-1/app.js
@@ -1,3 +1,17 @@
-/**
- * Write your challenge solution here
- */
+const toggleButton = document.getElementById("toggleButton");
+const statusText = document.getElementById("status");
+const bulb = document.getElementById("bulb");
+
+function toggleBulb() {
+  body.classList.toggle("dark-mode");
+  bulb.classList.toggle("off");
+  if (body.classList.contains("dark-mode")) {
+    (toggleButton.innerText = "Turn Off"),
+      (statusText.innerText = "Status: On");
+  } else {
+    (toggleButton.innerText = "Turn On"),
+      (statusText.innerText = "Status: Off");
+  }
+}
+
+toggleButton.addEventListener("click", toggleBulb);
