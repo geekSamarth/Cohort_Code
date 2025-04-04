174a1419a5723abfee8f43b0410bf0b62a84f196
geeksamarth <samarthagrawal1804@gmail.com>
2025-04-05 02:25:40 +0530

    add: DOM challenge-2 was added

--- DOM_Challenges/DOM_Challenges_1/challenge-2/app.js
+++ DOM_Challenges/DOM_Challenges_1/challenge-2/app.js
@@ -1,3 +1,22 @@
-/**
- * Write your challenge solution here
- */
+const mainHeading = document.getElementById("mainHeading");
+const redButton = document.getElementById("redButton");
+const greenButton = document.getElementById("greenButton");
+const blueButton = document.getElementById("blueButton");
+const purpleButton = document.getElementById("purpleButton");
+const resetButton = document.getElementById("resetButton");
+
+redButton.addEventListener("click", () => {
+  mainHeading.style.color = "red";
+});
+greenButton.addEventListener("click", () => {
+  mainHeading.style.color = "green";
+});
+blueButton.addEventListener("click", () => {
+  mainHeading.style.color = "blue";
+});
+purpleButton.addEventListener("click", () => {
+  mainHeading.style.color = "purple";
+});
+resetButton.addEventListener("click", () => {
+  mainHeading.style.color = "black";
+});
