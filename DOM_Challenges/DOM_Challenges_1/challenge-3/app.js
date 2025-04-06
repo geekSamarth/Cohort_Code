e66c40720fb1848aa5e665ab1ec0e5b8ba091193
geeksamarth <samarthagrawal1804@gmail.com>
2025-04-07 04:06:06 +0530

    add: challenge-3

--- DOM_Challenges/DOM_Challenges_1/challenge-3/app.js
+++ DOM_Challenges/DOM_Challenges_1/challenge-3/app.js
@@ -1,3 +1,21 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+nameInput.addEventListener("input", (event) => {
+  nameDisplay.innerText = event.target.value || "Not Provided";
+});
+jobInput.addEventListener("input", (event) => {
+  jobDisplay.innerText = event.target.value || "Not Provided";
+});
+ageInput.addEventListener("input", (event) => {
+  ageDisplay.innerText = event.target.value || "Not Provided";
+});
+bioInput.addEventListener("input", (event) => {
+  bioDisplay.innerText = event.target.value || "Not Provided";
+});
